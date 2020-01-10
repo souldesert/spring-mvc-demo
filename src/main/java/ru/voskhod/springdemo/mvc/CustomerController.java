f50ac80a68d5c4496a43ea5abcd4d4c5f81ebf84
Alexander Vasilkov <a.vasilkov@voskhod.ru>
2020-01-10 19:43:47 +0300

    Add pre-processing code with @InitBinder

--- src/main/java/ru/voskhod/springdemo/mvc/CustomerController.java
+++ src/main/java/ru/voskhod/springdemo/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package ru.voskhod.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,13 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add an initBinder method to convert trim empty strings
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model model) {
@@ -23,6 +33,8 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer customer,
             BindingResult bindingResult
     ) {
+
+        System.out.println("Last name: |" + customer.getLastName() + "|");
 
         if (bindingResult.hasErrors()) {
             return "customer-form";
