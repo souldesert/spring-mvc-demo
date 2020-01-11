cb21d70f235ae608c6c22a0d9b296b4e5e7f4020
Александр Васильков <y915@yandex.ru>
2020-01-11 17:17:41 +0300

    Configure resource bundle

--- src/main/java/ru/voskhod/springdemo/mvc/Customer.java
+++ src/main/java/ru/voskhod/springdemo/mvc/Customer.java
@@ -9,9 +9,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be >= 0")
     @Max(value = 10, message = "must be <= 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 letters/digits allowed")
     private String postalCode;
@@ -32,11 +33,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
