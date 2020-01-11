9410f07cab2dfc08c4957b8db37e67ccae5e9086
Александр Васильков <y915@yandex.ru>
2020-01-11 18:19:48 +0300

    @CourseCode annotation - finished

--- src/main/java/ru/voskhod/springdemo/mvc/validation/CourseCode.java
+++ src/main/java/ru/voskhod/springdemo/mvc/validation/CourseCode.java
@@ -1,6 +1,7 @@
 package ru.voskhod.springdemo.mvc.validation;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,4 +11,17 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode  {
+
+    // define default course code
+    public String value() default "LUV";
+
+    // define default error message
+    public String message() default "must start with LUV";
+
+    // define default groups
+    public Class<?>[] groups() default {};
+
+    // define default payloads
+    public Class<? extends Payload>[] payload() default {};
+
 }
