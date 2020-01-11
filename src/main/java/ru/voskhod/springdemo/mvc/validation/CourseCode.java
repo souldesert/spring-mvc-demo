63b1a38d0819e53ba452f51af0a7e81e87c5695a
Александр Васильков <y915@yandex.ru>
2020-01-11 18:02:52 +0300

    @CourseCode annotation created

---
+++ src/main/java/ru/voskhod/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,13 @@
+package ru.voskhod.springdemo.mvc.validation;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode  {
+}
---
+++ src/main/java/ru/voskhod/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,6 @@
+package ru.voskhod.springdemo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator {
+}
